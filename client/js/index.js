ad56ea0b0706f3f597a4c56b7842a67ed3f2e5c7
PlankCipher <plankcipher1@protonmail.com>
2021-08-17 09:02:10 +0200

    Add landing/home section

---
+++ client/js/index.js
@@ -0,0 +1,28 @@
+new TypeIt('#typeit', {
+  speed: 50,
+  deleteSpeed: 50,
+  loop: true,
+  cursorSpeed: 1000,
+  waitUntilVisible: true,
+  lifeLike: true,
+})
+  .type(
+    "Save time ... BOOK<span style='color: var(--primary-purple-color)'>IT</span>",
+  )
+  .pause(1000)
+  .delete()
+  .type(
+    "Choose<span style='color: var(--primary-purple-color)'> category</span>",
+  )
+  .pause(1000)
+  .delete(9)
+  .type("<span style='color: var(--primary-purple-color)'> place</span>")
+  .pause(1000)
+  .delete(6)
+  .type("<span style='color: var(--primary-purple-color)'> style</span>")
+  .pause(1000)
+  .delete()
+  .type("<span style='color: var(--primary-purple-color)'>Enjoy</span>")
+  .pause(1000)
+  .delete()
+  .go();
